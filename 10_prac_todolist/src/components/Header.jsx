e8d3c5d8981994b40709da423ce54b39ee258140
JaeranKIM <devjaeran@gmail.com>
2023-03-01 20:42:29 +0900

    react 연습 - todolist 5회차

---
+++ 10_prac_todolist/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div>My TodoList</div>
+        <div>React 연습 - TodoList 5회차</div>
+    </div>
+  )
+}
+
+export default Header
